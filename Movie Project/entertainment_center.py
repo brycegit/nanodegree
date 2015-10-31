958afb34c792a53128b7461eaa261eada3d98efb
brycegit <brycehdooley@gmail.com>
2015-10-31 15:03:21 -0400

    adding commentsand changing formatting  per feedback

--- Movie Project/entertainment_center.py
+++ Movie Project/entertainment_center.py
@@ -3,26 +3,41 @@ import media
 import fresh_tomatoes
 
 
-#movies list
+# movies list
 
-toy_story2 = media.Movie("Toy Story 2", "A story about toys, duh.",
-                        "http://ecx.images-amazon.com/images/I/518G5RSWQBL.jpg", "https://www.youtube.com/watch?v=Lu0sotERXhI",
-                        "1999", "<p>Tom Hanks</p><p>Tim Allen</p>")
+toy_story2 = media.Movie(
+    "Toy Story 2",
+    "A story about toys, duh.",
+    "http://ecx.images-amazon.com/images/I/518G5RSWQBL.jpg",
+    "https://www.youtube.com/watch?v=Lu0sotERXhI",
+    "1999",
+    "<p>Tom Hanks</p><p>Tim Allen</p>")
 
-terminator = media.Movie("Terminator", "A story about terminators, duh.",
+terminator = media.Movie(
+    "Terminator",
+    "A story about terminators, duh.",
+    "http://wrongsideoftheart.com/wp-content/gallery/posters-t/terminator_poster_07.jpg",
+    "https://www.youtube.com/watch?v=c4Jo8QoOTQ4",
+    "1984",
+    "<p>Arnold Schwarzenegger</p><p>Linda Hamilton</p>")
 
-                        "http://wrongsideoftheart.com/wp-content/gallery/posters-t/terminator_poster_07.jpg",
-                         "https://www.youtube.com/watch?v=c4Jo8QoOTQ4", "1984", "<p>Arnold Schwarzenegger</p><p>Linda Hamilton</p>")
+star_wars_v = media.Movie(
+    "The Empire Strikes Back",
+    "A story about stars, duh.",
+    "http://t1.gstatic.com/images?q=tbn:ANd9GcTtXwQAEDxEY3E9Nn78H96VZCjlV6hZWPlDd5IpVNyeuzO2vT17",
+    "http://www.youtube.com/watch?v=JNwNXF9Y6kY",
+    "1980",
+    "<p>Mark Hamill</p><p>Harrison Ford</p>")
 
-star_wars_v = media.Movie("The Empire Strikes Back", "A story about stars, duh.",
-                        "http://t1.gstatic.com/images?q=tbn:ANd9GcTtXwQAEDxEY3E9Nn78H96VZCjlV6hZWPlDd5IpVNyeuzO2vT17", "http://www.youtube.com/watch?v=JNwNXF9Y6kY",
-                        "1980", "<p>Mark Hamill</p><p>Harrison Ford</p>")
+lotr_3 = media.Movie(
+    "The Return of the King",
+    "A story about rings, duh.",
+    "http://www.gstatic.com/tv/thumb/movieposters/33156/p33156_p_v7_ak.jpg",
+    "https://youtu.be/Wmm5SNcjLvo",
+    "2003",
+    "<p>Elijah Wood</p><p>Viggo Mortensen</p>")
 
-lotr_3 = media.Movie("The Return of the King", "A story about rings, duh.",
-                        "http://www.gstatic.com/tv/thumb/movieposters/33156/p33156_p_v7_ak.jpg", "https://youtu.be/Wmm5SNcjLvo",
-                        "2003", "<p>Elijah Wood</p><p>Viggo Mortensen</p>")
-
-#/movies list
+# /movies list
 
 
 movies = [toy_story2, terminator, star_wars_v, lotr_3]
--- Movie Project/media.py
+++ Movie Project/media.py
@@ -1,8 +1,12 @@
 import webbrowser
 
+"""this class constructs a Movie object, and contains 6 attributes: title, storyline,
+poster image URL, youtube trailer URL, release date and cast list. """
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube, released_date, cast_list):
+        """this is the constructor method which takes the 6 attributes as inputs
+        and turns them into properties of the object created. """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
